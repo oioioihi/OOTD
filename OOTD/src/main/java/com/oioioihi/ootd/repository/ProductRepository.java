95997f3639daaac3869419eeb70b1db568056b2d
Boeun's <rnqhdms@gmail.com>
2024-08-15 16:20:35 +0900

    remove: Product Entity 복합키 제거

--- OOTD/src/main/java/com/oioioihi/ootd/repository/ProductRepository.java
+++ OOTD/src/main/java/com/oioioihi/ootd/repository/ProductRepository.java
@@ -1,13 +1,12 @@
 package com.oioioihi.ootd.repository;
 
 import com.oioioihi.ootd.model.entity.Product;
-import com.oioioihi.ootd.model.entity.ProductId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product, ProductId>, ProductRepositoryCustom, ProductRepositoryJdbcTemplate {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom, ProductRepositoryJdbcTemplate {
 
     Optional<List<Product>> findAllByBrandId(Long brandId);
 
